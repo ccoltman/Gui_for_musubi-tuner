c4e2bca86240953b7f96583c267e2bd2620b867a
ccoltman <ccoltman@elyptic.com>
2025-05-15 23:39:05 -0400

    Update update.bat

--- update.bat
+++ update.bat
@@ -4,5 +4,5 @@ pip install -U torch torchvision torchaudio --index-url https://download.pytorch
 pip install -U accelerate bitsandbytes xformers
 pip install https://github.com/sdbds/SageAttention-for-windows/releases/download/2.11_torch270+cu128/sageattention-2.1.1+cu128torch2.7.0-cp310-cp310-win_amd64.whl
 pip install -U triton-windows^<3.3
-install https://huggingface.co/880ga/flash-attn-274post1-cp310-cu128/resolve/main/flash_attn-2.7.4.post1+cu128torch2.7.0cxx11abiFALSE-cp310-cp310-win_amd64.whl
+pip install https://huggingface.co/880ga/flash-attn-274post1-cp310-cu128/resolve/main/flash_attn-2.7.4.post1+cu128torch2.7.0cxx11abiFALSE-cp310-cp310-win_amd64.whl
 pause
